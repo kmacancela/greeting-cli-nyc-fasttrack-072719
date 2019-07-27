0c3f1cf8f85483c58be8d3b0750e146bcd3e8ab6
Karina Macancela <karymacancela@gmail.com>
2019-07-27 19:00:51 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -3,6 +3,6 @@ def greeting(name)
   puts "Hello #{name}. It's nice to meet you."
 end
 
-# need to comment out the lines 
+# need to comment out lines 7 and 8 in order for  
 input = gets.chomp 
 greeting(input)
